6625dc2de4e51c6164a0d4e1790eca59a217ed06
Elias Marolla <elias.marolla@gmail.com>
2026-07-10 20:38:15 -0300

    semana 02 - Express

--- semana_02/jueves/server.js
+++ semana_02/jueves/server.js
@@ -1,3 +1,32 @@
-const model = require('../martes/ProductManager.js');
+const ProductManager = require('../martes/ProductManager.js');
+const express = require('express');
 const chalk = require('chalk');
+const model = new ProductManager();
+
+const port = 3000;
+
+const server = express();
+
+
 console.log(chalk.bgGreen('API REST'));
+
+server.listen(port, ()=>{
+    console.log(chalk.blue(`servidor en el puerto ${port}`));
+});
+
+server.get('/', (req, res)=>{
+    res.send('Hola desde express!!!');
+});
+
+server.get('/products', async(req, res)=>{
+    const list = await model.getProducts();
+    console.table(list);
+    res.json(list);
+});
+
+server.get('/products/:id', async(req, res)=>{
+    const id = req.params.id;
+    const data = await model.getProductsById(id);
+    console.table(data);
+    res.json(data);
+});
